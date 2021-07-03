532e07d08c59b52e2b69e2fe347419455156f6aa
Victor Costa <vpassos10@hotmail.com>
2021-07-03 11:13:22 -0300

    authentication oauth google

--- lib/shared/themes/app_text_styles.dart
+++ lib/shared/themes/app_text_styles.dart
@@ -11,7 +11,7 @@ class AppTextStyles {
   static final titleRegular = GoogleFonts.lexendDeca(
     fontSize: 20,
     fontWeight: FontWeight.w400,
-    color: AppColors.heading,
+    color: AppColors.background,
   );
   static final titleBoldHeading = GoogleFonts.lexendDeca(
     fontSize: 20,
